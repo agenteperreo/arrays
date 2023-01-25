e0ddd6cde87e840dcbd20e17c2ef8f76ac8f7526
igarcia <igarcar@iesnervion.es>
2023-01-25 14:38:56 +0100

    25/01/2023

---
+++ Arrays/src/eliminacion/Ejercicio1.java
@@ -0,0 +1,17 @@
+package eliminacion;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		//Declaración de variables
+		int numeros[]= new int[20];
+		
+		//Rellenamos el array
+		for(int i=0; i<numeros.length; i++) {
+			numeros[i]=(int)(Math.random()*100+1); 
+		}
+
+	}
+
+}
