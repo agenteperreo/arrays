2e93d89cf45d38ab49082b0f75f5b7b9045bbfa0
igarcia <igarcar@iesnervion.es>
2023-02-13 10:30:59 +0100

    13/02/2023

--- Arrays/src/buscaminas/Funciones.java
+++ Arrays/src/buscaminas/Funciones.java
@@ -1,45 +1,77 @@
 package buscaminas;
 
 public class Funciones {
-	
-	static final int numMinas=6;
-	
-	static void tablero(String[] auxiliar) {
-		
-		//Insertamos las minas
-		for(int i = 1; i <= numMinas; i++) {
-			int posMinas=(int)(Math.random()*auxiliar.length);
-			if(auxiliar[posMinas].equals("*")) {
+
+	// Cantidad de minas que hay en la partida
+	static final int numMinas = 6;
+
+	static void minas(String[] auxiliar) {
+
+		// Insertamos las minas
+		for (int i = 1; i <= numMinas; i++) {
+			int posMinas = (int) (Math.random() * auxiliar.length);
+			if (auxiliar[posMinas].equals("*")) {
 				i--;
 			}
-			auxiliar[posMinas]="*";
+			auxiliar[posMinas] = "*";
+		}
+
+	}
+
+	static void pistas(String[] auxiliar) {
+
+		// Si en la primera posicion no hay mina y en la segunda si
+		if (auxiliar[0].equals("-") && auxiliar[1].equals("*")) {
+			// Ponemos 1 en la primera posicion
+			auxiliar[0] = "1";
+			// Si no
+		} else {
+			// Ponemos 0 en la primera posicion
+			auxiliar[0] = "0";
+		}
+
+		// Si
+		if (auxiliar[auxiliar.length - 1].equals("-") && auxiliar[auxiliar.length - 2].equals("*")) {
+			auxiliar[auxiliar.length - 1] = "1";
+		} else {
+			auxiliar[auxiliar.length - 1] = "0";
 		}
 		
-		for(int i=0; i < auxiliar.length; i++) {
-			
+		// Recorremos el array auxiliar
+		for (int i = 1; i < auxiliar.length - 1; i++) {
+
+			if (auxiliar[i].equals("*")) {
+
+			} else if (auxiliar[i - 1].equals("*") && auxiliar[i + 1].equals("*")) {
+				auxiliar[i] = "2";
+			} else if (auxiliar[i - 1].equals("*") || auxiliar[i + 1].equals("*")) {
+				auxiliar[i] = "1";
+			} else {
+				auxiliar[i] = "0";
+			}
 		}
 	}
-	
-	static void posicionUsuario(String[] tablero, String[] auxiliar, int posicion) {
-		
-		//Le quitamos uno a la poscion para que sea funcional como indice
-		posicion-=1;
-		
-		//Si la posicion anterior o posterior es una mina
-		if(auxiliar[posicion-1].equals("*") || auxiliar[posicion+1].equals("*")) {
-			
-			//Mostramos que la posicion del usuario esta en contacto con una mina
-			tablero[posicion]="1";
-		
-			//	
-		}if(auxiliar[posicion-1].equals("*") && auxiliar[posicion+1].equals("*")){
-			tablero[posicion]="2";
-		}if(auxiliar[posicion].equals("*")){
-			tablero[posicion]="*";
-			System.out.println("¡¡¡BOOOOOM!!! Tocaste una mina");
-		} else {
-			tablero[posicion]="0";
+
+	static int posicionUsuario(String[] tablero, String[] auxiliar, int posicion) {
+
+		// Para transformar la posicion en indice para que sea funcional
+		posicion -= 1;
+
+		// Codigo de retorno de la funcion
+		int returnCode = 0;
+
+		// Si la posicion puesta por el usuario es una bomba
+		if (auxiliar[posicion].equals("*")) {
+
+			// Cambiamos el codigo de vuelta
+			returnCode = 1;
 		}
+
+		// Cambiamos la posicion del tablero por la posicion del auxiliar
+		tablero[posicion] = auxiliar[posicion];
+
+		// Cambiamos el codigo
+		return returnCode;
 	}
 
 }
--- Arrays/src/buscaminas/Main.java
+++ Arrays/src/buscaminas/Main.java
@@ -6,33 +6,72 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		//Creacion de arrays
-		String[] tablero= {"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"};
-		String[] auxiliar= {"-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-"};
-		
-		//Creamos la posicion que quiere descubrir el usuario
-		int pos;
-		
-		//Abrimos el escaner
-		Scanner sc = new Scanner(System.in);
-		
-		//Generamos las minas en el array auxiliar
-		Funciones.tablero(auxiliar);
-		
-		//Mostramos el tablero al usuario
-		System.out.println(Arrays.toString(tablero));
-		System.out.println(Arrays.toString(auxiliar));
-		
-		//Le pedimos la posición al usuario
-		System.out.print("Introduce la posicion que quieres descubrir: ");
-		pos=sc.nextInt();
-		
-		//LLamamos a la funcion par mostrar si ha tocado una mina y si hay alguna cerca o no
-		Funciones.posicionUsuario(tablero, auxiliar, pos);
 
-		//Mostramos el tablero con la posicion ya descubierta
+		// Creacion de arrays
+		String[] tablero = { "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+				"#", "#" };
+		String[] auxiliar = { "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-",
+				"-", "-" };
+
+		// Creamos la posicion que quiere descubrir el usuario
+		int pos = -1;
+		
+		//Cantidad de huecos abiertos
+		int movimientos = 0;
+		
+		//Codigo de retorno
+		int returnCode=0;
+
+		// Abrimos el escaner
+		Scanner sc = new Scanner(System.in);
+
+		// Generamos las minas en el array auxiliar
+		Funciones.minas(auxiliar);
+		
+		//Generamos las pistas en el array auxiliar
+		Funciones.pistas(auxiliar);
+
+		// Mostramos el tablero al usuario
 		System.out.println(Arrays.toString(tablero));
+		System.out.println();
+
+		while (movimientos != (tablero.length - Funciones.numMinas) && returnCode==0) {
+			// Le pedimos la posición al usuario
+			System.out.print("Introduce la posicion que quieres descubrir: ");
+			pos = sc.nextInt();
+
+			// LLamamos a la funcion par mostrar si ha tocado una mina y si hay alguna cerca
+			// o no
+			returnCode = Funciones.posicionUsuario(tablero, auxiliar, pos);
+
+			if (returnCode == 1) {
+				break;
+			}
+
+			// Mostramos el tablero con la posicion ya descubierta
+			System.out.println(Arrays.toString(tablero));
+
+			// Para que sea mas limpio
+			System.out.println();
+
+			// Añadimos 1 a los movimientos hechos
+			movimientos++;
+		}
+
+		// Si ha hecho los movimientos maximos
+		if (returnCode==1)
+			System.out.println("¡¡¡Ganaste!!!");
+		else
+			//Mostramos que le ha explotado
+			System.out.println("¡¡¡BOOOOOOM!!! Tas matao");
+
+		System.out.println(Arrays.toString(auxiliar));
+
+		// Mostramos que ha terminado el juego
+		System.out.println("Gracias por jugar.");
+
+		// Cerramos el escaner
+		sc.close();
 	}
 
 }
