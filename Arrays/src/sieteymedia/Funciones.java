012360a64211b91ae7983035cc88f9c2a30cd09e
igarcia <igarcar@iesnervion.es>
2023-02-15 14:37:38 +0100

    15/02/2023

--- Arrays/src/sieteymedia/Funciones.java
+++ Arrays/src/sieteymedia/Funciones.java
@@ -1,5 +1,59 @@
 package sieteymedia;
 
+import java.util.Scanner;
+
 public class Funciones {
+
+	static double puntosJ1 = 0;
+	static double puntosJ2 = 0;
+
+	static String tiradaJ1(String[] palos, String[] cartas) {
+
+		// Generamos una carta y un palo aleatorio
+		int carta = (int) (Math.random() * cartas.length);
+		int palo = (int) (Math.random() * palos.length);
+
+		// Si las cartas son la sota, caballo o rey
+		if (carta == 8 || carta == 9 || carta == 10) {
+			// Sumamos 0.5 a los puntos del jugador
+			puntosJ1 += 0.5;
+			// Si no
+		} else {
+			// Sumamos el valor de la carta a los puntos del jugador
+			puntosJ1 += Integer.parseInt(cartas[carta]);
+		}
+
+		// Devolvemos la cadena
+		return "Tu carta es: " + cartas[carta] + " de " + palos[palo];
+	}
+
+	static String tiradaJ2(String[] palos, String[] cartas) {
+
+		// Generamos una carta y un palo aleatorio
+		int carta = (int) (Math.random() * cartas.length);
+		int palo = (int) (Math.random() * palos.length);
+
+		// Si las cartas son la sota, caballo o rey
+		if (carta == 8 || carta == 9 || carta == 10) {
+			// Sumamos 0.5 a los puntos del jugador
+			puntosJ2 += 0.5;
+			// Si no
+		} else {
+			// Sumamos el valor de la carta a los puntos del jugador
+			puntosJ2 += Integer.parseInt(cartas[carta]);
+		}
+
+		// Devolvemos la cadena
+		return "Tu carta es: " + cartas[carta] + " de " + palos[palo];
+	}
+
+	static void menuTirada(String nombreJ, Scanner sc) {
+		// Menu de incio
+		System.out.println("------EMPEZAMOS-----");
+		System.out.println("Turno de " + nombreJ + "\nPresiona intro");
+		sc.nextLine();
+		
+		
+	}
 
 }
--- Arrays/src/sieteymedia/Main.java
+++ Arrays/src/sieteymedia/Main.java
@@ -1,9 +1,41 @@
 package sieteymedia;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Creamos los arrays con los palos y las cartas
+		String[] cartas = {"1", "2", "3", "4", "5", "6", "7", "sota", "caballo", "rey"};
+		String[] palos = {"Oro", "Basto", "Espada", "Copa"};
+		
+		//Nombres de los jugadores
+		String nombreJ1, nombreJ2, pedirCarta="";
+		
+		//Abrimos el escaner
+		Scanner sc = new Scanner(System.in);
+		
+		//Le pedimos los nombres al usuario
+		System.out.println("¿Cuales son vuestros nombres?: ");
+		System.out.print("J1 ");
+		nombreJ1= sc .nextLine();
+		System.out.print("J2 ");
+		nombreJ2=sc.nextLine();
+		
+		Funciones.menuTirada(nombreJ1, sc);
+		while(true) {
+			System.out.print("¿Quieres una carta?(si/no): ");
+			pedirCarta=sc.next();
+			
+			if(pedirCarta.equalsIgnoreCase("si")) {
+				System.out.println(Funciones.tiradaJ1(palos, cartas)); 
+			} else if(pedirCarta.equalsIgnoreCase("no")) {
+				break;
+			} else {
+				System.out.println();
+			}
+		}
 
 	}
 
